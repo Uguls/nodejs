674c7c496dd28d9c13e669a26751059205c5b9b1
Uguls <chobozaex@gmail.com>
2023-07-18 01:42:09 +0900

    Initial commit

---
+++ ch9/lecture/models/user.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true, // unique가 true여도 allowNull이 true면 null값은 고유한 값으로 인정하여 이메일 공백 가능
+                unique: true,
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true, // sns로 로그인 하는경우 비밀번호가 없을수도 있음
+            },
+            provider: {
+                type: Sequelize.STRING(10),
+                allowNull: false,
+                defaultValue: 'local',
+            },
+            snsId: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: true,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+
+    static associate(db) {
+        db.User.hasMany(db.Post);
+        db.User.belongsToMany(db.User, {
+            foreignKey: 'followingId',
+            as: 'Followers',
+            through: 'Follow',
+        });
+        db.User.belongsToMany(db.User, {
+            foreignKey: 'followerId',
+            as: 'Followings',
+            through: 'Follow',
+        });
+    }
+};
